6e29c53d73882654b0f04433028813e49216eee6
AliyaDevNova <124171140+aliyabatoolalvi@users.noreply.github.com>
2024-03-04 23:50:59 +0500

    added files data

--- app/src/main/java/com/example/aliyasstream/MainActivity.java
+++ app/src/main/java/com/example/aliyasstream/MainActivity.java
@@ -27,42 +27,42 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        private List<Article> getArticlesData() {
-            List<Article> articles=new ArrayList<>();
-            Article article=new Article(
+        private List<Blog> getArticlesData() {
+            List<Blog> blogs=new ArrayList<>();
+            Blog blog=new Blog(
                     "Everything You Need to Know About Camouflage, According to a U.S. Army Sniper Course Instructor",
                     "MATT CRISARA",
                     "FEB 10, 2024",
                     "Humans aren’t great at blending in with their environment. In the 20th century—after we ditched the rather barbaric style of fighting wars in fields, with line formations charging at one another—camouflage would become a vital tool. This has grown into a multi-billion-dollar industry meant to keep soldiers hidden on the battlefield, offering a significant tactical advantage.",
                     R.drawable.image1
             );
-            articles.add(article);
-            article=new Article(
+            articles.add(blog);
+            blog=new Blog(
                     "The LAV-25: Everything You Need to Know About the Marines’ Favorite Ride",
                     " KYLE MIZOKAMI",
                     "FEB 21, 2024",
                     "HuIts speed and firepower, coupled with its exceptional ability to deploy worldwide at a moment’s notice, makes the LAV-25 an ideal component of any crisis response force.mans aren’t great at blending in with their environment. In the 20th century—after we ditched the rather barbaric style of fighting wars in fields, with line formations charging at one another—camouflage would become a vital tool. This has grown into a multi-billion-dollar industry meant to keep soldiers hidden on the battlefield, offering a significant tactical advantage.",
                     R.drawable.image2
             );
-            articles.add(article);
-            article=new Article(
+            articles.add(blog);
+            blog=new Blog(
                     "The LAV-25: Everything You Need to Know About the Marines’ Favorite Ride",
                     " KYLE MIZOKAMI",
                     "FEB 21, 2024",
                     "HuIts speed and firepower, coupled with its exceptional ability to deploy worldwide at a moment’s notice, makes the LAV-25 an ideal component of any crisis response force.mans aren’t great at blending in with their environment. In the 20th century—after we ditched the rather barbaric style of fighting wars in fields, with line formations charging at one another—camouflage would become a vital tool. This has grown into a multi-billion-dollar industry meant to keep soldiers hidden on the battlefield, offering a significant tactical advantage.",
                     R.drawable.image2
             );
-            articles.add(article);
-            article=new Article(
+            articles.add(blog);
+            blog=new Blog(
                     "The LAV-25: Everything You Need to Know About the Marines’ Favorite Ride",
                     " KYLE MIZOKAMI",
                     "FEB 21, 2024",
                     "HuIts speed and firepower, coupled with its exceptional ability to deploy worldwide at a moment’s notice, makes the LAV-25 an ideal component of any crisis response force.mans aren’t great at blending in with their environment. In the 20th century—after we ditched the rather barbaric style of fighting wars in fields, with line formations charging at one another—camouflage would become a vital tool. This has grown into a multi-billion-dollar industry meant to keep soldiers hidden on the battlefield, offering a significant tactical advantage.",
                     R.drawable.image2
             );
-            articles.add(article);
+            articles.add(blog);
 
-            return articles;
+            return blogs;
         }
 
     }
